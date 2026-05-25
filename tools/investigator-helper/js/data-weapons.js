c008a1503fba9fdfa095f61bdc038b8a8016951e
くま。 <idenobu1012@gmail.com>
2026-05-25 19:34:42 -0400

    chara helper v2.3

--- tools/investigator-helper/js/data-weapons.js
+++ tools/investigator-helper/js/data-weapons.js
@@ -145,5 +145,992 @@ window.WEAPONS = [
     page: "",
     tags: ["近接戦闘武器", "ナイフ"],
     note: ""
-  }
+  },
+{
+    "id": "wood_axe_6e",
+    "name": "木斧",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "斧",
+    "skill": "斧",
+    "hit": "20",
+    "damage": "1D8+2+db",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "15",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "斧"
+    ],
+    "note": "記号「*」付き武器。"
+},
+{
+    "id": "hatchet_sickle_6e",
+    "name": "手斧、鎌",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "斧",
+    "skill": "斧",
+    "hit": "20",
+    "damage": "1D6+1+db",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "12",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "斧"
+    ],
+    "note": "記号「*」付き武器。"
+},
+{
+    "id": "large_club_cricket_bat_poker_6e",
+    "name": "大きい棍棒／クリケットのバット／火かき棒",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "杖",
+    "skill": "杖",
+    "hit": "25",
+    "damage": "1D8+db",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "20",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "杖"
+    ],
+    "note": ""
+},
+{
+    "id": "small_club_baton_6e",
+    "name": "小さい棍棒／警棒",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "杖",
+    "skill": "杖",
+    "hit": "25",
+    "damage": "1D6+db",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "15",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "杖"
+    ],
+    "note": ""
+},
+{
+    "id": "tactical_baton_6e",
+    "name": "タクティカル・バトン",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "杖",
+    "skill": "杖",
+    "hit": "25",
+    "damage": "1D8+db",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "25",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "杖"
+    ],
+    "note": ""
+},
+{
+    "id": "sword_cane_6e",
+    "name": "仕込み杖*",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "杖",
+    "skill": "杖",
+    "hit": "20",
+    "damage": "1D6+db",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "10",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "杖"
+    ],
+    "note": "記号「*」付き武器。"
+},
+{
+    "id": "spear_6e",
+    "name": "槍",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "槍／銃剣",
+    "skill": "槍／銃剣",
+    "hit": "15",
+    "damage": "1D10+db",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "15",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "槍／銃剣"
+    ],
+    "note": "記号「*」付き武器。"
+},
+{
+    "id": "bayonet_6e",
+    "name": "銃剣",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "槍／銃剣",
+    "skill": "槍／銃剣",
+    "hit": "10",
+    "damage": "1D6+db",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "12",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "槍／銃剣"
+    ],
+    "note": "記号「*」付き武器。"
+},
+{
+    "id": "katana_6e",
+    "name": "日本刀",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "日本刀",
+    "skill": "日本刀",
+    "hit": "15",
+    "damage": "1D10+db",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "20",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "日本刀"
+    ],
+    "note": ""
+},
+{
+    "id": "wakizashi_6e",
+    "name": "脇差",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "日本刀",
+    "skill": "日本刀",
+    "hit": "15",
+    "damage": "1D6+1+db",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "15",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "日本刀"
+    ],
+    "note": ""
+},
+{
+    "id": "shinai_6e",
+    "name": "竹刀",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "日本刀",
+    "skill": "日本刀",
+    "hit": "15",
+    "damage": "スタン+db",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "2",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "日本刀"
+    ],
+    "note": ""
+},
+{
+    "id": "liuyedao_6e",
+    "name": "柳葉刀",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "青竜刀",
+    "skill": "青竜刀",
+    "hit": "10",
+    "damage": "1D8+db",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "20",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "青竜刀"
+    ],
+    "note": ""
+},
+{
+    "id": "qinglong_dadao_6e",
+    "name": "青竜大刀",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "青竜刀",
+    "skill": "青竜刀",
+    "hit": "5",
+    "damage": "2D6+db",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "30",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "青竜刀"
+    ],
+    "note": ""
+},
+{
+    "id": "nunchaku_6e",
+    "name": "双節棍",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "節棍",
+    "skill": "節棍",
+    "hit": "10",
+    "damage": "1D8+db",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "20",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "節棍"
+    ],
+    "note": ""
+},
+{
+    "id": "sansetsukon_6e",
+    "name": "三節棍",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "節棍",
+    "skill": "節棍",
+    "hit": "5",
+    "damage": "1D10+db",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "25",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "節棍"
+    ],
+    "note": ""
+},
+{
+    "id": "naginata_6e",
+    "name": "薙刀",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "個別技能",
+    "skill": "薙刀",
+    "hit": "15",
+    "damage": "1D8+db",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "15",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "個別技能",
+        "薙刀"
+    ],
+    "note": ""
+},
+{
+    "id": "tachi_6e",
+    "name": "太刀",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "個別技能",
+    "skill": "太刀",
+    "hit": "5",
+    "damage": "2D8+db",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "30",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "個別技能",
+        "太刀"
+    ],
+    "note": ""
+},
+{
+    "id": "kusarigama_6e",
+    "name": "鎖鎌※",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "個別技能",
+    "skill": "鎖鎌",
+    "hit": "5",
+    "damage": "1D6または〈巻きつき〉",
+    "range": "3m",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "4",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "個別技能",
+        "鎖鎌"
+    ],
+    "note": "記号「※」付き武器。"
+},
+{
+    "id": "blackjack_6e",
+    "name": "ブラックジャック",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "個別技能",
+    "skill": "こぶし／パンチ",
+    "hit": "〈こぶし／パンチ〉",
+    "damage": "1D8+db",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "4",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "個別技能",
+        "こぶし／パンチ"
+    ],
+    "note": ""
+},
+{
+    "id": "garrote_6e",
+    "name": "絞殺ひも※",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "個別技能",
+    "skill": "絞殺ひも",
+    "hit": "15",
+    "damage": "首締め",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "1",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "個別技能",
+        "絞殺ひも"
+    ],
+    "note": "記号「※」付き武器。"
+},
+{
+    "id": "whip_6e",
+    "name": "ムチ※",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "個別技能",
+    "skill": "ムチ",
+    "hit": "5",
+    "damage": "1D3または〈巻きつき〉",
+    "range": "3m",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "4",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "個別技能",
+        "ムチ"
+    ],
+    "note": "記号「※」付き武器。"
+},
+{
+    "id": "burning_torch_6e",
+    "name": "燃えているたいまつ",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "個別技能",
+    "skill": "たいまつ",
+    "hit": "10+衣服の火%",
+    "damage": "1D6",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "15",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "個別技能",
+        "たいまつ"
+    ],
+    "note": ""
+},
+{
+    "id": "stun_gun_projectile_6e",
+    "name": "スタンガン（射出型）※",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "個別技能",
+    "skill": "拳銃",
+    "hit": "〈拳銃〉",
+    "damage": "スタン",
+    "range": "5m",
+    "attacks": "1",
+    "ammo": "さまざま",
+    "durability": "8",
+    "malfunction": "95",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "個別技能",
+        "スタンガン",
+        "拳銃"
+    ],
+    "note": "記号「※」付き武器。"
+},
+{
+    "id": "stun_gun_contact_6e",
+    "name": "スタンガン（接触型）※",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "個別技能",
+    "skill": "こぶし／パンチ",
+    "hit": "〈こぶし／パンチ〉",
+    "damage": "スタン",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "さまざま",
+    "durability": "7",
+    "malfunction": "97",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "個別技能",
+        "スタンガン",
+        "こぶし／パンチ"
+    ],
+    "note": "記号「※」付き武器。"
+},
+{
+    "id": "tear_gas_6e",
+    "name": "催涙ガス※",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "個別技能",
+    "skill": "こぶし／パンチ",
+    "hit": "〈こぶし／パンチ〉",
+    "damage": "2D10分間スタン",
+    "range": "2m",
+    "attacks": "1",
+    "ammo": "25発",
+    "durability": "4",
+    "malfunction": "00",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "個別技能",
+        "催涙ガス",
+        "こぶし／パンチ"
+    ],
+    "note": "記号「※」付き武器。"
+},
+{
+    "id": "electrified_wire_100v_6e",
+    "name": "電気の通じた導線、100ボルト",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "個別技能",
+    "skill": "電気修理",
+    "hit": "〈電気修理〉",
+    "damage": "1D8+スタン",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "6、ヒューズ",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "個別技能",
+        "電気修理"
+    ],
+    "note": ""
+},
+{
+    "id": "electrified_wire_200v_6e",
+    "name": "電気の通じた導線、200ボルト",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "個別技能",
+    "skill": "電気修理",
+    "hit": "〈電気修理〉",
+    "damage": "2D8+スタン",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "6、ヒューズ",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "個別技能",
+        "電気修理"
+    ],
+    "note": ""
+},
+{
+    "id": "chainsaw_6e",
+    "name": "チェーンソー※",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "近接戦闘武器",
+    "category": "個別技能",
+    "skill": "チェーンソー",
+    "hit": "20",
+    "damage": "2D8",
+    "range": "タッチ",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "20",
+    "malfunction": "97",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "近接戦闘武器",
+        "個別技能",
+        "チェーンソー"
+    ],
+    "note": "記号「※」付き武器。"
+},
+{
+    "id": "thrown_stone_6e",
+    "name": "投石",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "飛び道具",
+    "category": "飛び道具（個別技能）",
+    "skill": "投擲",
+    "hit": "〈投擲〉",
+    "damage": "1D2+1/2db",
+    "range": "20",
+    "attacks": "2",
+    "ammo": "―",
+    "durability": "20",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "飛び道具",
+        "個別技能",
+        "投擲"
+    ],
+    "note": ""
+},
+{
+    "id": "javelin_6e",
+    "name": "投げ槍",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "飛び道具",
+    "category": "飛び道具（個別技能）",
+    "skill": "投擲",
+    "hit": "〈投擲〉",
+    "damage": "1D8+1/2db",
+    "range": "[STR]m",
+    "attacks": "1/2",
+    "ammo": "―",
+    "durability": "15",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "飛び道具",
+        "個別技能",
+        "投擲"
+    ],
+    "note": ""
+},
+{
+    "id": "bow_6e",
+    "name": "ボウ*",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "飛び道具",
+    "category": "飛び道具（個別技能）",
+    "skill": "ボウ",
+    "hit": "10",
+    "damage": "1D8",
+    "range": "30",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "10",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "飛び道具",
+        "個別技能",
+        "ボウ"
+    ],
+    "note": "記号「*」付き武器。"
+},
+{
+    "id": "crossbow_6e",
+    "name": "クロスボウ*",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "飛び道具",
+    "category": "飛び道具（個別技能）",
+    "skill": "クロスボウ",
+    "hit": "20",
+    "damage": "1D8+2",
+    "range": "50",
+    "attacks": "1/2",
+    "ammo": "―",
+    "durability": "15",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "飛び道具",
+        "個別技能",
+        "クロスボウ"
+    ],
+    "note": "記号「*」付き武器。"
+},
+{
+    "id": "sling_6e",
+    "name": "スリング*",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "飛び道具",
+    "category": "飛び道具（個別技能）",
+    "skill": "スリング",
+    "hit": "1",
+    "damage": "1D4+1/2db",
+    "range": "60",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "―",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "飛び道具",
+        "個別技能",
+        "スリング"
+    ],
+    "note": "記号「*」付き武器。"
+},
+{
+    "id": "kyudo_bow_6e",
+    "name": "弓（弓道）*",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "飛び道具",
+    "category": "飛び道具（個別技能）",
+    "skill": "弓（弓道）",
+    "hit": "10",
+    "damage": "1D6+1",
+    "range": "90m",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "4",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "飛び道具",
+        "個別技能",
+        "弓道"
+    ],
+    "note": "記号「*」付き武器。"
+},
+{
+    "id": "competition_archery_6e",
+    "name": "競技用アーチェリー",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "飛び道具",
+    "category": "飛び道具（個別技能）",
+    "skill": "アーチェリー",
+    "hit": "20",
+    "damage": "1D8",
+    "range": "70m",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "4",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "飛び道具",
+        "個別技能",
+        "アーチェリー"
+    ],
+    "note": ""
+},
+{
+    "id": "shuriken_6e",
+    "name": "手裏剣",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "飛び道具",
+    "category": "飛び道具（個別技能）",
+    "skill": "手裏剣",
+    "hit": "25",
+    "damage": "1D3+1/2db",
+    "range": "20m",
+    "attacks": "2",
+    "ammo": "―",
+    "durability": "2",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "飛び道具",
+        "個別技能",
+        "手裏剣"
+    ],
+    "note": ""
+},
+{
+    "id": "boomerang_6e",
+    "name": "ブーメラン",
+    "ruleType": "6e",
+    "ruleLabels": [
+        "6版"
+    ],
+    "group": "飛び道具",
+    "category": "飛び道具（個別技能）",
+    "skill": "投擲",
+    "hit": "〈投擲〉",
+    "damage": "1D8+1/2db",
+    "range": "20m",
+    "attacks": "1",
+    "ammo": "―",
+    "durability": "4",
+    "malfunction": "―",
+    "source": "",
+    "sourceShort": "",
+    "page": "",
+    "tags": [
+        "飛び道具",
+        "個別技能",
+        "投擲"
+    ],
+    "note": ""
+}
 ];
