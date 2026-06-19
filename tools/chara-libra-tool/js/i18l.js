6ae6b4c452907cd9a51cb8cd635dd635df9e3bf2
くま。 <idenobu1012@gmail.com>
2026-06-19 12:53:08 -0400

    Redesign Chara Libra library page

---
+++ tools/chara-libra-tool/js/i18l.js
@@ -0,0 +1,1 @@
+window.CharaLibraI18n={lang:"ja",dict:{ja:{life:{alive:"生存",lost:"ロスト",retired:"引退",inactive:"保留",npc:"NPC"}},en:{life:{alive:"Alive",lost:"Lost",retired:"Retired",inactive:"Inactive",npc:"NPC"}}}};
