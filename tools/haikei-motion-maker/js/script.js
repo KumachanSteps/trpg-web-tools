d2d750a5b0b20023ef8ed02a08b074e79b3bdcc4
くま。 <idenobu1012@gmail.com>
2026-06-30 19:30:41 -0400

    delete haikeimotion files

--- tools/haikei-motion-maker/js/script.js
+++
@@ -1,6 +0,0 @@
-(() => {
-  'use strict';
-
-  // Bootstrap file reserved for future cross-tool integrations.
-  // Core motion logic lives in main.js; keyboard handling lives in shortcut.js.
-})();
